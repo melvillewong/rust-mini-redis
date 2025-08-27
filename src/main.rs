8e58248b2f36918eddc2565059fca8270f8f5469
melvillewong <melvillewong14@gmail.com>
2025-08-27 22:48:04 +0100

    Fixed #2, #3: Completed basic echo server with tokio async

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,39 @@
-use std::net::TcpListener;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+};
 
-fn main() -> std::io::Result<()> {
-    let listen = TcpListener::bind("127.0.0.1:6379")?;
-    for stream in listen.incoming() {
-        let stream = stream?;
-        println!("{stream:?} connected");
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    // Bind server to address
+    let listen = TcpListener::bind("127.0.0.1:6379").await?;
+    println!("Listening on localhost:6379");
+
+    while let Ok((mut socket, addr)) = listen.accept().await {
+        println!("New client connected: {}", addr);
+
+        tokio::spawn(async move {
+            let mut buf = [0u8; 512];
+
+            loop {
+                match socket.read(&mut buf).await {
+                    Ok(0) => {
+                        println!("Client {} disconnected", addr);
+                        return;
+                    }
+                    Ok(byte_read) => {
+                        if let Err(e) = socket.write_all(&buf[..byte_read]).await {
+                            eprintln!("Failed to write to {}: {}", addr, e);
+                            return;
+                        }
+                    }
+                    Err(e) => {
+                        eprintln!("Failed to read from {}: {}", addr, e);
+                        return;
+                    }
+                }
+            }
+        });
     }
     Ok(())
 }
